dc505d4f7ea82201a715040a051acd3ca4844545
Artur Klimovich <a.klimovich@noda.live>
2024-07-30 17:10:56 +0200

    test: add title

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -26,6 +26,7 @@ export default async function Home() {
                         src/app/page.tsx
                     </code>
                 </p>
+                <h1>HUY</h1>
                 <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
                     <a
                         className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
