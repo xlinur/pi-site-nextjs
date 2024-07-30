769212caa0655c3be3d2cb82c51e4f9d4bb9a534
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-07-30 14:42:50 +0300

    chore: just adds files for fun

---
+++ README.md
@@ -0,0 +1,42 @@
+NOTE: This repository does use npm workspaces.
+
+
+#### Start NextJS project
+
+```bash
+npm run dev -w nextjs
+```
+
+
+#### Start Strapi project development mode
+
+```bash
+npm run develop -w strapi
+```
+
+
+#### Pre-setup instructions
+
+```bash
+chmod +x ./packages/strapi/init-db.sh
+```
+
+NOTE: This command has to be executed because this bash script does create a database in postgres' container.
+
+
+#### Run docker compose by following command
+
+```bash
+docker-compose --env-file ./packages/strapi/.env -f docker-compose.yml up -d
+```
+
+
+#### Contribution
+
+```bash
+npm run commit
+```
+
+TODO list:
+
+[] - move out .env file into root for docker-compose.yml 
---
+++ packages/nextjs/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "next/core-web-vitals",
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+    ],
+    ignorePatterns: ["dist", ".eslintrc.js"],
+    parser: "@typescript-eslint/parser",
+    rules: {
+        "no-unused-vars": [
+            "off",
+            { vars: "all", args: "after-used", ignoreRestSiblings: false },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+                caughtErrorsIgnorePattern: "^_",
+            },
+        ],
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto",
+            },
+        ],
+        "@typescript-eslint/ban-ts-comment": "warn",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "react-refresh/only-export-components": "off",
+    },
+    globals: {
+        chrome: "readonly",
+    },
+};
