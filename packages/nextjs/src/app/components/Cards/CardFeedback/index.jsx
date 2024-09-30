63b1336e0611ac8526ffd3aa84686661de7e6ede
Artur Klimovich <a.klimovich@noda.live>
2024-10-01 00:43:44 +0200

    feedback modal

--- packages/nextjs/src/app/components/Cards/CardFeedback/index.jsx
+++ packages/nextjs/src/app/components/Cards/CardFeedback/index.jsx
@@ -1,13 +1,16 @@
+import { Suspense } from 'react';
 import Markdown from 'react-markdown';
-import Button from '../../Button';
-import styles from './styles.module.scss';
 import clsx from 'clsx';
+
+import Button from '@/app/components/Button';
+import ModalComponent from '@/app/components/Modal';
+
+import styles from './styles.module.scss';
 
 function CardFeedback(props) {
   const {
     essence,
     content,
-    link,
     company,
     authorName,
     authorPosition,
@@ -23,32 +26,56 @@ function CardFeedback(props) {
   );
 
   return (
-    <article className={cardClsx}>
-      <div className={styles.cardContent}>
-        <b className={styles.cardContentEssence}>{essence}</b>
+    <>
+      <article className={cardClsx}>
+        <div className={styles.cardContent}>
+          <b className={styles.cardContentEssence}>{essence}</b>
 
-        <Markdown className={styles.text}>{content}</Markdown>
-      </div>
-
-      <div className={styles.btnInlineUser}>
-        <div className={styles.cardUser}>
-          <h5 className={styles.cardUserTitle}>{company}</h5>
-          <div className={styles.cardUserAbout}>
-            <div className={styles.cardUserName}>{authorName}</div>
-            <div className={styles.cardUserPosition}>{authorPosition}</div>
-          </div>
+          <Markdown className={styles.text}>{content}</Markdown>
         </div>
 
-        <Button
-          theme="text"
-          withIcon
-          iconRight
-          href={link}
-          className={styles.linkReadMore}
-          name={readAllBtn}
-        />
-      </div>
-    </article>
+        <div className={styles.btnInlineUser}>
+          <div className={styles.cardUser}>
+            <h5 className={styles.cardUserTitle}>{company}</h5>
+            <div className={styles.cardUserAbout}>
+              <div className={styles.cardUserName}>{authorName}</div>
+              <div className={styles.cardUserPosition}>{authorPosition}</div>
+            </div>
+          </div>
+
+          <Button
+            theme="text"
+            withIcon
+            iconRight
+            className={styles.linkReadMore}
+            name={readAllBtn}
+            openModal
+          />
+        </div>
+      </article>
+
+      <Suspense fallback={null}>
+        <ModalComponent>
+          <section className={styles.modalFeedbackBody}>
+            <header>
+              <div className={styles.cardUser}>
+                <h5 className={styles.cardUserTitle}>{company}</h5>
+                <div className={styles.cardUserAbout}>
+                  <div className={styles.cardUserName}>{authorName}</div>
+                  <div className={styles.cardUserPosition}>
+                    {authorPosition}
+                  </div>
+                </div>
+              </div>
+            </header>
+
+            <div className={styles.modalFeedbackBodyContent}>
+              <Markdown>{content}</Markdown>
+            </div>
+          </section>
+        </ModalComponent>
+      </Suspense>
+    </>
   );
 }
 
