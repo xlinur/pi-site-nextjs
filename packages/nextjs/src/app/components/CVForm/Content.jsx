ebcc4a8cdbe3f33551986018338764396f5e5208
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2024-10-03 12:41:08 +0300

    fix: fixs base64 for cv getting content

--- packages/nextjs/src/app/components/CVForm/Content.jsx
+++ packages/nextjs/src/app/components/CVForm/Content.jsx
@@ -29,7 +29,7 @@ function convertFileToBase64(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
-    reader.onloadend = () => resolve(reader.result);
+    reader.onloadend = () => resolve(reader.result.split(',')[1]);
     reader.onerror = (error) => reject(error);
     reader.readAsDataURL(file);
   });
