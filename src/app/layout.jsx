838c13074faac9811f6846a2937bd9de87231227
e-klimovich <pochta.korobka@gmail.com>
2024-10-22 20:34:25 +0200

    error boundary added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@
 import { Roboto } from 'next/font/google';
 import GoogleCaptchaWrapper from '@/app/components/GoogleCaptchaWrapper';
 import GoogleAnalytics from '@/app/components/GoogleAnalytics';
+import ErrorBoundary from '@/app/components/ErrorBoundary';
 import '@/app/styles/globals.scss';
 
 const roboto = Roboto({
@@ -14,8 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body className={roboto.className}>
-        <GoogleAnalytics />
-        <GoogleCaptchaWrapper>{children}</GoogleCaptchaWrapper>
+        <ErrorBoundary>
+          <GoogleAnalytics />
+          <GoogleCaptchaWrapper>{children}</GoogleCaptchaWrapper>
+        </ErrorBoundary>
       </body>
     </html>
   );
